30059b70732bdd49b4457f6370ac0a419815b377
lakatoshentertainment <lakatoshentertainment@gmail.com>
2026-08-18 12:32:28 -0400

    Update text.js

--- config/text.js
+++ config/text.js
@@ -86,7 +86,7 @@ window.TEXT = {
 
   contact: {
     heading: "Contact",
-    facebook: { label: "Facebook", href: "https://www.facebook.com/yourpage" },
+    facebook: { label: "Facebook", href: "https://www.facebook.com/profile.php?id=61593162192955" },
     instagram: { label: "Instagram", href: "https://www.instagram.com/yourhandle" },
     email: "lakatoshentertainment@gmail.com",
     emailHref: "mailto:lakatoshentertainment@gmail.com"
